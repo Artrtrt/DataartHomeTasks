cc04bc92fc623974a85f38e139845c5183b12981
Artrtrt <artretard@mail.ru>
2021-10-26 19:38:33 +0300

    fixed the task

--- vrn-java-05__3_/src/main/java/ru/dataart/academy/java/Calculator.java
+++ vrn-java-05__3_/src/main/java/ru/dataart/academy/java/Calculator.java
@@ -11,14 +11,17 @@ public class Calculator {
      * @return - sum of firstNumber + secondNumber
      */
     public Integer getSum(List<Integer> firstNumber, List<Integer> secondNumber) {
-        int sum = 0;
-        for (int i = 0; i < firstNumber.size(); i++) {
-            sum += firstNumber.get(i) * Math.pow(10, i);
+        int num1 = listInNumber(firstNumber);
+        int num2 = listInNumber(secondNumber);
+        return num1 + num2;
+    }
+
+    public Integer listInNumber(List<Integer> list) {
+        int number = 0;
+        for (int i = 0; i < list.size(); i++) {
+            number += list.get(i) * Math.pow(10, i);
         }
-        for (int i = 0; i < secondNumber.size(); i++) {
-            sum += secondNumber.get(i) * Math.pow(10, i);
-        }
-        return sum;
+        return number;
     }
 
     /**
@@ -29,12 +32,8 @@ public class Calculator {
      */
     public <T> List<T> getOddElements(List<T> list) {
         var newList = new ArrayList<T>();
-        int sw = 1;
-        for (T el: list) {
-            sw = (sw == 1) ? 0 : 1;
-            if (sw == 0) {
-                newList.add(el);
-            }
+        for (int i = 0; i < list.size(); i += 2) {
+                newList.add(list.get(i));
         }
         return newList;
     }
@@ -47,6 +46,12 @@ public class Calculator {
      * [1, 2, 3 , 4] -> [1, 4]
      */
     public <T> List<T> getBounds(List<T> list) {
+        if (list.size() == 1) {
+            return list;
+        }
+        if (list.isEmpty()) {
+            return Collections.emptyList();
+        }
         var newList = new ArrayList<T>();
         newList.add(list.get(0));
         newList.add(list.get(list.size() - 1));
